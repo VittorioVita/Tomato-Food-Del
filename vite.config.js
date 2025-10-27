06e5a7dd89b07e7017fdca21035bb017d652c595
vittorio caporale <vittoriocaporalevita@gmail.com>
2025-10-27 17:01:14 +0100

    Fix: Imposto il base path per GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // AGGIUNGI QUESTA RIGA 👇
+  base: "/Tomato-Food-Del/",
   plugins: [react()],
 })
